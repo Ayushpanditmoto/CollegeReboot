188a49f240b5fe7297434b331291c21dda737677
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-11-24 23:17:56 +0530

    added react router dom and many pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Components/Home';
+import Login from './Pages/Components/Login';
+import Profile from './Pages/Components/Profile';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
