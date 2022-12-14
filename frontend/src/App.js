727c6bf3e260b942a1c259f20c7d74e12937c36c
savvyteamwork <115590643+savvyteamwork@users.noreply.github.com>
2022-12-14 19:44:43 +0500

    #10 create a forgot password page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Signup from './Pages/Signup/Signup';
-import Login from './Pages/Login/Login';
-import Profile from './Pages/Profile';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./Pages/Signup/Signup";
+import Login from "./Pages/Login/Login";
+import Profile from "./Pages/Profile";
+import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot" element={<ForgotPassword />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
