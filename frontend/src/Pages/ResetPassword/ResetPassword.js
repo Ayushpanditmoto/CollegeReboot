3d9a18589d5d6586e282814afc2fc2b1baea8a22
JohnnyOhms <officialjohn662@gmail.com>
2022-12-22 00:16:12 +0100

    react fragment added to the reset password page

--- frontend/src/Pages/ResetPassword/ResetPassword.js
+++ frontend/src/Pages/ResetPassword/ResetPassword.js
@@ -2,29 +2,25 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { Formik, Form } from "formik";
 import PasswordInput from "./PasswordInput";
-import {  redirect } from "react-router-dom";
+import { redirect } from "react-router-dom";
 import * as Yup from "yup";
 
 const PasswordValidationSchema = Yup.object({
-
   password: Yup.string()
-    .min(8, 'Password is Too Short!')
-    .max(50, 'Password is Too Long!')
+    .min(8, "Password is Too Short!")
+    .max(50, "Password is Too Long!")
     .required("Password is required"),
 
-  confirmPassword: Yup.string()  
-    .min(8, 'Password is Too Short!')
-    .max(50, 'Password is Too Long!')
-    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+  confirmPassword: Yup.string()
+    .min(8, "Password is Too Short!")
+    .max(50, "Password is Too Long!")
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
     .required("Confirm Password is required"),
-
 });
-
-
 
 function ResetPassword() {
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword ] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
@@ -33,78 +29,78 @@ function ResetPassword() {
   const handleConfirmPasswordChange = (e) => {
     setConfirmPassword(e.target.value);
   };
-  
-  const clearInput = () =>{
-    setPassword('')
-    setConfirmPassword('')
-  }
 
-  const handleSubmit = async (e) =>{
-   
-     e.preventDefault();
+  const clearInput = () => {
+    setPassword("");
+    setConfirmPassword("");
+  };
 
-      const response = fetch('/reset-password',{
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          password: password,
-          Authorization: 'TestReturnedToken',
-        })
-      })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-      console.log( response );
+    const response = fetch("/reset-password", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        password: password,
+        Authorization: "TestReturnedToken",
+      }),
+    });
 
-      if (!response.ok) {           
-        const message = `An error has occured: ${response.status}`;
-        throw new Error(message);
-      }
+    console.log(response);
 
-      return redirect("/");  
-    
-  }
+    if (!response.ok) {
+      const message = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+
+    return redirect("/");
+  };
 
   return (
-    <>
+    <React.Fragment>
       <ResetContainer>
         <ResetHeader></ResetHeader>
         <ResetForm>
-        <h2>Change Password</h2>    
-        <p>Pick a strong password</p>
+          <h2>Change Password</h2>
+          <p>Pick a strong password</p>
           <Formik
             enableReinitialize
             initialValues={{
               password,
-              confirmPassword
+              confirmPassword,
             }}
             validationSchema={PasswordValidationSchema}
-            onSubmit={ handleSubmit }
+            onSubmit={handleSubmit}
           >
-            <Form>             
-                <PasswordInput
-                  name="password"
-                  placeholder="New Password"
-                  type="password"
-                  onChange={ handlePasswordChange }
-                />
+            <Form>
+              <PasswordInput
+                name="password"
+                placeholder="New Password"
+                type="password"
+                onChange={handlePasswordChange}
+              />
 
-                <PasswordInput
-                  name="confirmPassword"
-                  placeholder="Confirm new Password"
-                  type="password"
-                  onChange={ handleConfirmPasswordChange }
-                />
-                
-                <ButtonWrapper>
-                  <button type="reset" onClick={ clearInput }>Cancel</button>
-                  <button type="submit">Continue</button>
-                </ButtonWrapper>
-              </Form>            
-          </Formik>          
+              <PasswordInput
+                name="confirmPassword"
+                placeholder="Confirm new Password"
+                type="password"
+                onChange={handleConfirmPasswordChange}
+              />
+
+              <ButtonWrapper>
+                <button type="reset" onClick={clearInput}>
+                  Cancel
+                </button>
+                <button type="submit">Continue</button>
+              </ButtonWrapper>
+            </Form>
+          </Formik>
         </ResetForm>
       </ResetContainer>
-    </>
+    </React.Fragment>
   );
 }
 
@@ -126,15 +122,15 @@ const ResetHeader = styled.div`
   margin: 100px 0 40px;
   flex-direction: column;
   justify-content: center;
-  align-items: center;  
+  align-items: center;
 `;
 
 const ButtonWrapper = styled.div`
-  display:flex;
+  display: flex;
   justify-content: right;
-  margin-top:8px;
+  margin-top: 8px;
   border-top: solid 1px var(--bg-third);
-  width:100%;
+  width: 100%;
 `;
 
 const ResetForm = styled.div`
@@ -147,33 +143,32 @@ const ResetForm = styled.div`
   padding: 1rem;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: left;  
+  align-items: left;
 
   h2 {
     font-size: 1.6rem;
     font-weight: 900;
     color: #0088ff;
-    padding-bottom:8px;
+    padding-bottom: 8px;
     margin-bottom: 8px;
     border-bottom: solid 1px var(--bg-third);
   }
- 
-  p{
-    padding:10px 0px;
+
+  p {
+    padding: 10px 0px;
   }
 
-  hr{
-    height:1px;
-    width:100%;
+  hr {
+    height: 1px;
+    width: 100%;
   }
 
   form {
     display: flex;
     flex-direction: column;
     align-items: center;
-    
   }
-  
+
   button {
     width: 110px;
     padding: 8px 5px;
@@ -186,10 +181,10 @@ const ResetForm = styled.div`
     font-weight: 600;
     cursor: pointer;
     margin: 10px 5px;
-  } 
+  }
 
-  [type="reset"]{
-    background-color:var(--bg-third);
+  [type="reset"] {
+    background-color: var(--bg-third);
     color: var(--color-secondary);
   }
 `;
