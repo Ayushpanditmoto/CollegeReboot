a8da620c912060dba1e37afddd4ea0c4e15c7148
Ayush Kumar Pandit <ayushpanditmoto@gmail.com>
2022-12-22 02:00:29 +0530

    chore: fixed merge conflict

--- frontend/src/Pages/Signup/Signup.js
+++ frontend/src/Pages/Signup/Signup.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { AiOutlineClose, AiOutlineInfoCircle } from 'react-icons/ai';
