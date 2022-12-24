c4c935f022fd66927a27bbcd1f3335a500a709b6
Ayush Kumar Pandit <ayushpanditmoto@gmail.com>
2022-12-24 05:37:01 +0530

    refactor: removed backend

--- backend/Middleware/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = (req, res, next) => {
-  console.log(
-    `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
-  );
-  next();
-};
-
-module.exports = logger;
--- backend/Utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const generateToken = (user, expires) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE || expires,
-  });
-};
-
-module.exports = generateToken;
