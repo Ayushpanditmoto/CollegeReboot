e5b4c2c51577507b12067474b2e26252fcfda39f
Ayush Kumar Pandit <ayushpanditmoto@gmail.com>
2023-01-11 17:53:14 +0530

    feat route fixed done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Forgot from "./Pages/ForgotPassword/ForgotPassword";
 import NotFound from "./Pages/NotFound/NotFound";
 import Home from "./Pages/Home/Home";
 import Reset from "./Pages/ResetPassword/ResetPassword";
+import EmailReset from "./Pages/EmailReset/EmailReset";
+import ResetLink from "./Pages/ResetPassword/ResetLink";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot" element={<Forgot />} />
         <Route path="/reset" element={<Reset />} />
+        <Route path="/emailreset" element={<EmailReset />} />
+        <Route path="/reset-link" element={<ResetLink />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/:path" element={<NotFound />} />
       </Routes>
